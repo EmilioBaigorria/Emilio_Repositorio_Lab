20d9ac5597da837123e7868a21eec7ab40eab67b
EmilioBaigorria <141742553+EmilioBaigorria@users.noreply.github.com>
2023-10-10 12:26:48 -0300

    Update PracticaConStrings10.java

--- PracticosEnClaseStrings/src/PracticaConStrings10.java
+++ PracticosEnClaseStrings/src/PracticaConStrings10.java
@@ -1,37 +1,38 @@
+// ej 10 strings
 import java.util.Scanner;
 
-public class PracticaConStrings10 {
-    public static void main(String[] args) {
-        //10 -Dado un texto terminado en punto, mostrar la palabra con más consonantes seguidas.
-        Scanner input = new Scanner(System.in);
-        int temp,ini_pos=0,consonant_amount=0,consonant_amount_temp=0;
-        String word,word_temp,max_consonant_word="";
-        System.out.println("Ingrese una frase y con mas consonantes seguidas");
-        word=input.nextLine();
-        word=(word.trim())+" ";
-        temp=word.indexOf(" ");
-        while (temp != -1 ){
-            word_temp=word.substring(ini_pos,temp);
-            ini_pos=temp;
-            for (int i=0 ;i<word_temp.length();i++){
-                System.out.println("p");
-                if ("bcdfghjklmnñpqrstvwxyz".contains(String.valueOf(word_temp.charAt(i)))){
-                    System.out.println(String.valueOf(word_temp.charAt(i)));
-                    if (String.valueOf(word_temp.charAt(i)).equals(String.valueOf("bcdfghjklmnñpqrstvwxyz".contains(String.valueOf(word_temp.charAt(i+1)))))){
-                        System.out.println("j");
-                        consonant_amount++;
-                    }
-                }
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner reader=new Scanner(System.in);
+    String frase, palabraConMasConsonantes="", palabraConMasConsonantesTemp="";
+    int contadorDeEspacios=0,contadorDeConsonantes=0, contadorDeConsonantesTemp=0;
+    String[] fraseSeparada;
+    System.out.println("Ingrese una frase, que termine en punto:");
+    frase=reader.nextLine();
+    fraseSeparada = frase.split("\\ ");
+    
+  for(int i=0; i<frase.length(); i++){
+          if ("bcdfghjklmnpqrstvwxyz".contains(String.valueOf(frase.charAt(i)))) {
+            contadorDeConsonantesTemp++;
+          } else {
+            if (contadorDeConsonantesTemp > contadorDeConsonantes) {
+              contadorDeConsonantes = contadorDeConsonantesTemp;
+              contadorDeConsonantesTemp = 0;
+            } else {
+              contadorDeConsonantesTemp = 0;
             }
-            if (consonant_amount>consonant_amount_temp){
-                max_consonant_word=word_temp;
-                consonant_amount_temp=consonant_amount;
+          }
+        if (frase.charAt(i)==' '){
+          palabraConMasConsonantesTemp = fraseSeparada[contadorDeEspacios];
+          
+          if (contadorDeConsonantesTemp>contadorDeConsonantes){
+            palabraConMasConsonantes=palabraConMasConsonantesTemp;
+            contadorDeConsonantesTemp=0;
             }
-            consonant_amount=0;
-            temp=word.indexOf(" ",temp+1);
+          contadorDeEspacios++;
         }
-        System.out.println("La palabra con mas cosonantes seguidas fue: "+max_consonant_word+" con "+consonant_amount_temp+" consonantes seguidas");
-
-
+        
     }
+  }
 }
